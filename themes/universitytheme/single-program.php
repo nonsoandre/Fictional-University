bc552bdf94da2348c5bd7c0b39b937c49d75e58c
nonsoandre <nonsoandre@gmail.com>
2022-05-22 09:58:25 +0100

    Reducing Duplicate with Template Parts in Events
    - learnt how to use template parts

--- themes/universitytheme/single-program.php
+++ themes/universitytheme/single-program.php
@@ -102,50 +102,17 @@ if ($relatedProfessors->have_posts()) {
 if ($homePageEvents->have_posts()) {
 ?>
 <?php
- echo '<hr class="section-rule">';
- echo '<h2 class="headline headline--medium">Upcoming ' . get_the_title() . ' Events</h2>'; 
-  while( $homePageEvents->have_posts()) {
-    $homePageEvents->the_post();
+    echo '<hr class="section-rule">';
+    echo '<h2 class="headline headline--medium">Upcoming ' . get_the_title() . ' Events</h2>'; 
+    
+    while( $homePageEvents->have_posts()) {
+        $homePageEvents->the_post();
 
-?>
-    <div class="event-summary">
-        <a class="event-summary__date t-center" href="#">
-            <span class="event-summary__month">
-            <?php
-                //the_field('event_date');   // this comes out in a format we wouldnt like
-                //We use a built in PHP time function
-                $eventDate = new DateTime(get_field('event_date'));
-                echo $eventDate->format('M')
-            ?>
-            </span>
-            <span class="event-summary__day">
-            <?php 
-                echo $eventDate->format('d');
-            ?>
-            </span>
-        </a>
-        <div class="event-summary__content">
-            <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-            <p> 
-            <?php 
-                if (has_excerpt()){
-                    echo get_the_excerpt();
-                }else{
-                    echo wp_trim_words(get_the_content(), 18); 
-                };
-                ?>
-            <!-- <?php echo wp_trim_words( get_the_content(), 18 ) ?>  -->
-            
-            
-            <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
-        </div>
-    </div>
+        get_template_part( '/template_parts/event-content' );
 
-<?php
-  } //end if statement 
-?>
-<?php
-} //end while loop for event summary
+    } //end while statement 
+
+} //end if loop for event summary
 ?>
 </div>
 
